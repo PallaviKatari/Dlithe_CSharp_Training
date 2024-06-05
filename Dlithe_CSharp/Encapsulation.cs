2962078d78746d343bb8f0c14490b360e18691fe
PallaviKatari <pallavikatari@gmail.com>
2024-06-05 21:02:07 +0530

    commit_5.6.24

---
+++ Dlithe_CSharp/Encapsulation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dlithe_CSharp
+{
+    /// <summary>
+    /// Encapsulation - Bundling Data
+    /// </summary>
+    internal class Encapsulation
+    {
+        /// <summary>
+        /// What is a Summary - Explanation about your method
+        /// summary - XML(eXtensible Markup language) format
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            EncapsulationDemo demo = new EncapsulationDemo();
+            //demo.id = 1;
+            //demo.name= "test";
+            //Console.WriteLine($"{demo.id}:{demo.name}");
+            demo.Id = 10;
+            demo.Name = "John Doe";
+            Console.WriteLine($"{demo.Id}:{demo.Name}");
+            Console.WriteLine(demo.Name);
+            Console.ReadLine();
+
+        }
+    }
+
+    internal class EncapsulationDemo
+    {
+        int id;
+        string name;
+
+        /// <summary>
+        /// Property - prop tab - get/set
+        /// </summary>
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+
+    }
+}
---
+++ Dlithe_CSharp/Polymorphism.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dlithe_CSharp
+{
+    /// <summary>
+    /// Polymorphism-Method Overloading,Method Overriding
+    /// </summary>
+    internal class Polymorphism
+    {
+        static void Main(string[] args)
+        {
+            Overloading overloading = new Overloading();
+            overloading.Add();
+            overloading.Add(100);
+            overloading.Add("John Doe");
+            overloading.Add(1, "Peter");
+            overloading.Add("Jane Doe", 2);
+            overloading.Add(200);
+            overloading.Add(3, "Paul");
+            Console.ReadLine();
+        }
+    }
+
+    /// <summary>
+    /// Method - many forms -> add() -> various forms
+    /// Overloading - Rules -> Method Signature must be implemented according
+    /// Method Signature - different datatypes, no: of parameters, sequence/order of parameters
+    /// </summary>
+    internal class Overloading
+    {
+        public void Add()
+        {
+            Console.WriteLine("Method with no parameter");
+        }
+
+        public void Add(int a)
+        {
+            Console.WriteLine("Method with int parameter:" + a);
+        }
+        public void Add(string name)
+        {
+            Console.WriteLine("Method with string parameter:" + name);
+        }
+        public void Add(int id, string name)
+        {
+            Console.WriteLine($"Method with int:{id},string parameter:{name}");
+        }
+
+        public void Add(string ename,int eid)
+        {
+            Console.WriteLine($"Method with string parameter:{ename},int:{eid}");
+        }
+    }
+    
+}
